9434b006d60597e82da914f615a4a4c819fd52ce
Phillip Redfern <philllip347@gmail.com>
2018-04-28 00:02:22 +0200

    setting up
    change index location
    add index html view

--- public/index.php
+++ public/index.php
@@ -1,3 +1,5 @@
 <?php
 session_start();
 require '../src/autoload.php';
+
+require '../view/index-view.php';
---
+++ view/index-view.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+	<title>Le Chiro Qui Roule</title>
+
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Exo">
+
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+	<header>
+		<div class="container">
+
+			<h1>Le Chiro Qui Roule</h1>
+
+		</div>
+
+		<nav class="NavBar navbar navbar-expand-lg navbar-light bg-light" data-spy="affix" data-offset-top="200">
+			<div class="containerNav">
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    		         <span class="navbar-toggler-icon"></span>
+  			     </button>
+
+				<div class="MenuNavBar collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav mr-auto">
+						<li class="menuNavItem nav-item active">
+							<a class="itemDeroulants nav-link" href="file:///P:/Projet%20Site%20Stage/index.html">Acceuil <span class="sr-only"></span></a>
+						</li>
+						<li class="menuNavItem nav-item dropdown">
+							<a class="itemDeroulants NavForm nav-link dropdown-toggle" href="file:///P:/Projet%20Site%20Stage/chiropraxie.html" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			          	                  La chiropraxie
+			        	              </a>
+							<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+								<a class="itemDeroulants dropdown-item" href="chiropraxie.html#Histoire_et_définition">Histoire et définition</a>
+								<a class="itemDeroulants dropdown-item" href="chiropraxie.html#Reconnaissance_légale_en_France">Reconnaissance légale en France</a>
+								<a class="itemDeroulants dropdown-item" href="chiropraxie.html#Reconnaissance_dans_le_monde">Reconnaissance dans le monde </a>
+								<a class="itemDeroulants dropdown-item" href="chiropraxie.html#La_formation_en_chiropraxie">La formation en chiropraxie </a>
+								<a class="itemDeroulants dropdown-item" href="chiropraxie.html#La_recherche_en_chiropraxie">La recherche en chiropraxie</a>
+							</div>
+						</li>
+						<li class="menuNavItem nav-item dropdown">
+							<a class="itemDeroulants NavForm nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          				      		Les Soins Chiropratiques
+          				        </a>
+							<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+								<a class="itemDeroulants dropdown-item" href="soins.html#Pour_Qui">Pour Qui ? </a>
+								<a class="itemDeroulants dropdown-item" href="soins.html#Les_symptomes_pour_consulter">Les symptômes pour consulter</a>
+								<div class="dropdown-divider"></div>
+								<a class="itemDeroulants dropdown-item" href="soins.html#Deroulement_d'une_séance">Déroulement d’une séance </a>
+								<a class="itemDeroulants dropdown-item" href="soins.html#Tarifs">Tarifs</a>
+								<a class="itemDeroulants dropdown-item" href="soins.html#Mutuelles_remboursant_les_soins">Mutuelles remboursant les soins </a>
+							</div>
+						</li>
+						<li class="menuNavItem nav-item dropdown">
+							<a class="itemDeroulants NavForm nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          			          	Votre Chiropracteur
+          				        </a>
+							<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+								<a class="itemDeroulants dropdown-item" href="chiropracteur.html#Son_parcours">Son parcours </a>
+								<a class="itemDeroulants dropdown-item" href="chiropracteur.html#Seance_a_domicile_a_velo">Séance à domicile à vélo </a>
+								<div class="dropdown-divider"></div>
+								<a class="itemDeroulants dropdown-item" href="chiropracteur.html#Contacts">Contacts</a>
+							</div>
+						</li>
+						<li class="menuNavItem nav-item">
+							<a class="itemDeroulants NavForm nav-link" href="wcbt.html">Le World Chiropractic Bike Tour</a>
+						</li>
+						<li class="menuNavItem nav-item">
+							<a class="itemDeroulants NavForm nav-link" href="#">Connexion</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	</header>
+
+
+	<main>
+
+		<div class="AlexandreVelo">
+
+			<img src="P:\Projet Site Stage\img_alexandre_velo.jpg" width="100%" height="100%">
+
+		</div>
+
+		<div class="container">
+
+			<p>Bienvenue sur le site du Chiro Qui Roule, Le Chiro Qui Roule, c’est l’idée innovante d’Alexandre Chassagne, Chiropracteur depuis Novembre 2016 après avoir passé 6 années d’études à l’Institut Franco Européen de Chiropraxie (lien URL vers le site de
+				mon école https://www.ifec.net/). En effet, jeune chiropracteur de 26 ans, décide depuis le premier trimestre 2018 de sillonner les rues de Laval agglomération à vélo accompagné de sa petite table de soins portable posé sur une remorque dans le but
+				de prodiguer des soins chiropratiques à domicile tout en étant éco-responsable.
+			</p>
+
+
+
+		</div>
+	</main>
+
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3">
+					<h5>Alexandre Chassagne</h5>
+					<p></p>
+				</div>
+				<div class="col-lg-5">
+					<h5 class="Liens">Liens</h5>
+					<ul class="listeliens">
+						<li class="listecate"><a href=""><i class="chevron fa fa-chevron-right" aria-hidden="true"></i>Histoire et définition</a></li>
+						<li class="listecate"><a href=""><i class="chevron fa fa-chevron-right" aria-hidden="true"></i>Reconnaissance légale en France</a></li>
+						<li class="listecate"><a href=""><i class="chevron fa fa-chevron-right" aria-hidden="true"></i>Reconnaissance dans le monde </a></li>
+						<li class="listecate"><a href=""><i class="chevron fa fa-chevron-right" aria-hidden="true"></i>La formation en chiropraxie </a></li>
+						<li class="listecate"><a href=""><i class="chevron fa fa-chevron-right" aria-hidden="true"></i>La recherche en chiropraxie</a></li>
+					</ul>
+				</div>
+				<div class="col-lg-4">
+					<h5 class="Contact">Contact</h5>
+					<p class="ContactFooter"><i class="telephone fas fa-phone" data-fa-transform="flip-v flip-h"></i>07 68 48 10 02</p>
+					<p><i class="chevron fab fa-facebook fa-spin"></i>Le Chiro qui roule</p>
+					<p><i class="chevron fab fa-instagram fa-spin"></i>Lechiroquiroule</p>
+					<h5 class="RDV">RDV</h5>
+					<div class="RDVButton">
+						<a href="#" class="btn btn-secondary btn-sm" role="button" aria-pressed="true">Prendre rendez-vous</a>
+					</div>
+					<p class="RDVtxt">N'hésitez pas à prendre rendez-vous dès maintenant.</p>
+
+				</div>
+			</div>
+		</div>
+	</footer>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+</body>
+
+</html>
