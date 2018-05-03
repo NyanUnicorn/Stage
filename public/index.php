a1b4d28176bc6c145839d72de7b1db4604b6857b
Phillip Redfern <philllip347@gmail.com>
2018-05-03 09:58:10 +0200

    cleaning
    move php from style folder to models folder

--- public/index.php
+++ public/index.php
@@ -2,6 +2,6 @@
 session_start();
 require '../src/autoload.php';
 
-require '/style/style.php';
+require '../models/style.php';
 
 require '../view/index-view.php';
--- public/style/style.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function getStylesheet($_name){
-  $styleSheet = '<link rel="stylesheet" type="text/css" href="style/' . $_name . '.css">';
-  return $styleSheet;
-
-}
