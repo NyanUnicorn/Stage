ef7e3403519ab1a0e35a586f92cb3196f671bcfd
NyanUnicorn <per1110@yahoo.com>
2018-05-07 01:25:42 +0200

    side nav
    incomplete style

--- view/side-nav-view.php
+++ view/side-nav-view.php
@@ -1,6 +1,18 @@
 <div class="Bandeau nav nav-pills nav-stacked" data-spy="affix" data-offset-top="212">
   <div class="flex-container">
-    <fieldset><span>Telephone : 07 68 48 10 02</span></fieldset>
+    <fieldset>
+      <div class="row">
+
+<div class="phoneLabel col-lg-12 col-xl-4">
+    <span>Telephone:</span>
+</div>
+<div class="phoneNumber col-lg-12 col-xl-8">
+    <span> 07 68 48 10 02</span>
+</div>
+
+
+      </div>
+    </fieldset>
     <button type="button" class="RdvBandeau btn .btn-primary btn-sm">Prendre Rendez-Vous</button>
   </div>
 </div>
