51e6469b4f2e4e7b155918e60a3b58959624f822
Phillip Redfern <philllip347@gmail.com>
2018-05-11 12:08:20 +0200

    supp scroll horizontal
    plus de row bootstrap san-padding pour enlever la scrol bar horizontale

--- view/footer-view.php
+++ view/footer-view.php
@@ -1,49 +1,70 @@
 <footer>
-  <div class="row">
-    <div class="col-xs-1 col-lg-2">
+<div class="row no-pad">
+  <div class="col-xs-0 col-lg-2">
+
+  </div>
+  <div class="col-xs-12 col-lg-8">
+
+
+    <div class="row">
+      <div class="col-xs-1 col-lg-0">
+
+      </div>
+      <div class="col-xs-10 col-lg-12">
+
+
+
+          <div class="row">
+            <div class="col-lg-4">
+                <h5>Alexandre Chassagne</h5>
+                <p>Docteur of chiropractic, formée à l'Institut Franco Européen de Chiropraxie.</p>
+                <p>Chiropracteur à domicile éco responsable sur Laval agglomération.</p>
+          <p>Fondateur de l'organisation humanitaire World Chiropractic Bike Tour.</p>
+              </div>
+              <div class="col-lg-5">
+              <h5 class="Liens">Liens</h5>
+              <ul class="listeliens">
+                <li class="listecate"><a href="chiropraxie.html#Histoire_et_définition"><i class="chevron fa fa-chevron-right" aria-hidden="true"></i>Histoire et définition</a></li>
+                <li class="listecate"><a href="chiropraxie.html#Reconnaissance_légale_en_France"><i class="chevron fa fa-chevron-right" aria-hidden="true"></i>Reconnaissance légale en France</a></li>
+                <li class="listecate"><a href="chiropraxie.html#Reconnaissance_dans_le_monde"><i class="chevron fa fa-chevron-right" aria-hidden="true"></i>Reconnaissance dans le monde </a></li>
+                <li class="listecate"><a href="chiropraxie.html#La_formation_en_chiropraxie"><i class="chevron fa fa-chevron-right" aria-hidden="true"></i>La formation en chiropraxie </a></li>
+                <li class="listecate"><a href="chiropraxie.html#La_recherche_en_chiropraxie"><i class="chevron fa fa-chevron-right" aria-hidden="true"></i>La recherche en chiropraxie</a></li>
+              </ul>
+            </div>
+            <div class="col-lg-3">
+              <h5 class="Contact">Contact</h5>
+              <p class="ContactFooter"><i class="telephone fas fa-phone" data-fa-transform="flip-v flip-h" ></i>07 68 48 10 02</p>
+              <p><i class="chevron fab fa-facebook fa-spin"></i>Le Chiro qui roule</p>
+              <p><i class="chevron fab fa-instagram fa-spin"></i>Lechiroquiroule</p>
+                <h5 class="RDV">RDV</h5>
+                <div class="RDVButton">
+                  <a href="#" class="btn btn-secondary btn-sm" role="button" aria-pressed="true">Prendre rendez-vous</a>
+                </div>
+                <p class="RDVtxt">N'hésitez pas à prendre rendez-vous dès maintenant.</p>
+
+          </div>
+          </div>
+
 
     </div>
-    <div class="col-xs-10 col-lg-8">
 
+      <div class="col-xs-1 col-lg-0">
 
+      </div>
+  </div>
 
-        <div class="row">
-          <div class="col-lg-4">
-              <h5>Alexandre Chassagne</h5>
-              <p>Docteur of chiropractic, formée à l'Institut Franco Européen de Chiropraxie.</p>
-              <p>Chiropracteur à domicile éco responsable sur Laval agglomération.</p>
-        <p>Fondateur de l'organisation humanitaire World Chiropractic Bike Tour.</p>
-            </div>
-            <div class="col-lg-5">
-            <h5 class="Liens">Liens</h5>
-            <ul class="listeliens">
-              <li class="listecate"><a href="chiropraxie.html#Histoire_et_définition"><i class="chevron fa fa-chevron-right" aria-hidden="true"></i>Histoire et définition</a></li>
-              <li class="listecate"><a href="chiropraxie.html#Reconnaissance_légale_en_France"><i class="chevron fa fa-chevron-right" aria-hidden="true"></i>Reconnaissance légale en France</a></li>
-              <li class="listecate"><a href="chiropraxie.html#Reconnaissance_dans_le_monde"><i class="chevron fa fa-chevron-right" aria-hidden="true"></i>Reconnaissance dans le monde </a></li>
-              <li class="listecate"><a href="chiropraxie.html#La_formation_en_chiropraxie"><i class="chevron fa fa-chevron-right" aria-hidden="true"></i>La formation en chiropraxie </a></li>
-              <li class="listecate"><a href="chiropraxie.html#La_recherche_en_chiropraxie"><i class="chevron fa fa-chevron-right" aria-hidden="true"></i>La recherche en chiropraxie</a></li>
-            </ul>
-          </div>
-          <div class="col-lg-3">
-            <h5 class="Contact">Contact</h5>
-            <p class="ContactFooter"><i class="telephone fas fa-phone" data-fa-transform="flip-v flip-h" ></i>07 68 48 10 02</p>
-            <p><i class="chevron fab fa-facebook fa-spin"></i>Le Chiro qui roule</p>
-            <p><i class="chevron fab fa-instagram fa-spin"></i>Lechiroquiroule</p>
-              <h5 class="RDV">RDV</h5>
-              <div class="RDVButton">
-                <a href="#" class="btn btn-secondary btn-sm" role="button" aria-pressed="true">Prendre rendez-vous</a>
-              </div>
-              <p class="RDVtxt">N'hésitez pas à prendre rendez-vous dès maintenant.</p>
-
-        </div>
-        </div>
 
 
   </div>
-    </div>
-    <div class="col-xs-1 col-lg-2">
+  <div class="col-xs-0 col-lg-2">
 
-    </div>
+  </div>
+</div>
+
+
+
+
+
 
   </div>
   </footer>
--- view/index-view2.php
+++ view/index-view2.php
@@ -32,32 +32,57 @@
 
   		</div>
 
-  		<div class="row">
-<div class="col-lg-2">
+
+			<div class="row no-pad">
+	<div class="col-xs-0 col-lg-2">
+
+	</div>
+	<div class="col-xs-12 col-lg-8">
+
+
+		<div class="row">
+<div class="col-xs-1 col-lg-0">
 
 </div>
 
-<div class="col-lg-8">
+<div class="col-xs-10 col-lg-12">
 
 
-  			<h2 class="Presentation">Présentation</h2>
+			<h2 class="Presentation">Présentation</h2>
 
-  				<p>Bienvenue sur le site du Chiro Qui Roule,
-  					Le Chiro Qui Roule, c’est l’idée innovante d’Alexandre Chassagne, Chiropracteur depuis Novembre 2016 après avoir passé 6 années d’études à l’Institut Franco Européen de Chiropraxie (lien URL vers le site de mon école https://www.ifec.net/).
-  					En effet, jeune chiropracteur de 26 ans, décide depuis le premier trimestre 2018 de sillonner les rues de Laval agglomération à vélo accompagné de sa petite table de soins portable posé sur une remorque dans le but de prodiguer des soins chiropratiques à domicile tout en étant éco-responsable.
-  				</p>
+				<p>Bienvenue sur le site du Chiro Qui Roule,
+					Le Chiro Qui Roule, c’est l’idée innovante d’Alexandre Chassagne, Chiropracteur depuis Novembre 2016 après avoir passé 6 années d’études à l’Institut Franco Européen de Chiropraxie (lien URL vers le site de mon école https://www.ifec.net/).
+					En effet, jeune chiropracteur de 26 ans, décide depuis le premier trimestre 2018 de sillonner les rues de Laval agglomération à vélo accompagné de sa petite table de soins portable posé sur une remorque dans le but de prodiguer des soins chiropratiques à domicile tout en étant éco-responsable.
+				</p>
 
-  			<h2 class="Presentation">Témoignages</h2>
+			<h2 class="Presentation">Témoignages</h2>
 
-  			<h2 class="Presentation">Actualités</h2>
+			<h2 class="Presentation">Actualités</h2>
 
-        </div>
+			</div>
 
-        <div class="col-lg-2">
+			<div class="col-xs-1 col-lg-0">
 
-        </div>
+			</div>
 
-      </div>
+		</div>
+
+
+
+	</div>
+	<div class="col-xs-0 col-lg-2">
+
+	</div>
+	</div>
+
+
+
+
+
+
+ </div>
+
+
 	</main>
 
 	<?php
