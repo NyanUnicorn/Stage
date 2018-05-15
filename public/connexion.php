c6bd05bb60cb3a46266bbce6640bcc1ada09240e
Phillip Redfern <philllip347@gmail.com>
2018-05-15 16:32:47 +0200

    User et déplacement des outils de formulaires dans une classe de service
    pas encor testé, un require a décommenter

--- public/connexion.php
+++ public/connexion.php
@@ -2,7 +2,9 @@
 session_start();
 require '../src/autoload.php';
 
-require '../lib/form.php';
+//require '../lib/form.php';
+use Service\Form;
+
 require '../models/style.php';
 require '../models/connections.php';
 require '../models/image.php';
