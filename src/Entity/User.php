0e1a1db930effa81bf2cf444725a4c052737c420
Phillip Redfern <philllip347@gmail.com>
2018-05-15 14:51:01 +0200

    ++

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,8 +4,27 @@ namespace Entity;
 
 
 class User{
+  private $id;
   private $nom;
   private $pre;
-  
+  private $email;
+  private $date_nais;
+  private $date_crea;
+  private $age;
+  private $phone;
+  private $adresse;
+  private $comp_adr;
+  private $cd_postale;
+  private $ville;
+  private $profession;
+  private
+
+  public function __construct($_nom, $_pre){
+
+  }
+
+  public function getNom(){
+    return $nom;
+  }
 
 }
