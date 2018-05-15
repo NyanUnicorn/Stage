c39f19e6adc9b5896d7a4b1192ce906f88542667
Martin Guyot <martin-guyot@hotmail.fr>
2018-05-15 14:45:07 +0200

    includeExternalFoot()

--- view/chiropraxie-view.php
+++ view/chiropraxie-view.php
@@ -137,43 +137,10 @@
 
 	</main>
 
-	<footer>
-    	<div class="container">
-      		<div class="row">
-        		<div class="col-lg-3">
-          			<h5>Alexandre Chassagne</h5>
-          			<p></p>
-          		</div>
-          		<div class="col-lg-5">
-		          <h5 class="Liens">Liens</h5>
-		          <ul class="listeliens">
-		            <li class="listecate"><a href=""><i class="chevron fa fa-chevron-right" aria-hidden="true"></i>Histoire et définition</a></li>
-		            <li class="listecate"><a href=""><i class="chevron fa fa-chevron-right" aria-hidden="true"></i>Reconnaissance légale en France</a></li>
-		            <li class="listecate"><a href=""><i class="chevron fa fa-chevron-right" aria-hidden="true"></i>Reconnaissance dans le monde </a></li>
-		            <li class="listecate"><a href=""><i class="chevron fa fa-chevron-right" aria-hidden="true"></i>La formation en chiropraxie </a></li>
-		            <li class="listecate"><a href=""><i class="chevron fa fa-chevron-right" aria-hidden="true"></i>La recherche en chiropraxie</a></li>
-		          </ul>
-		        </div>
-		        <div class="col-lg-4">
-		        	<h5 class="Contact">Contact</h5>
-		        	<p class="ContactFooter"><i class="telephone fas fa-phone" data-fa-transform="flip-v flip-h" ></i>07 68 48 10 02</p>
-		        	<p><i class="chevron fab fa-facebook fa-spin"></i>Le Chiro qui roule</p>
-		        	<p><i class="chevron fab fa-instagram fa-spin"></i>Lechiroquiroule</p>
-		          	<h5 class="RDV">RDV</h5>
-		          	<div class="RDVButton">
-		          		<a href="#" class="btn btn-secondary btn-sm" role="button" aria-pressed="true">Prendre rendez-vous</a>
-		          	</div>
-		          	<p class="RDVtxt">N'hésitez pas à prendre rendez-vous dès maintenant.</p>
+	<?php
 
-		      </div>
-        	</div>
-
-	    </div>
-
-  </footer>
-		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
-</body>
+		require 'footer-view.php';
+		includeExternalFoot();
+	 ?>
 
 </html>
--- view/wcbt.php
+++ view/wcbt.php
@@ -5,11 +5,14 @@
 	<title>Le Chiro Qui Roule</title>
 
 	<meta charset="utf-8">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
-	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Exo">
 
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<?php
+			includeExternalHead();
+			echo getStylesheet('style');
+			echo getStylesheet('style_form');
+			echo getStylesheet('style_menu');
+	 ?>
+
 </head>
 <body>
 	<?php
@@ -19,6 +22,10 @@
 	 ?>
 
 	<main>
+
+		<div class="encart">
+			<?php require 'side-nav-view.php'; ?>
+		</div>
 
 		<div class="container">
 
@@ -59,11 +66,10 @@ C’est une lettre que votre chiropracteur préparera régulièrement pour vous 
 	<?php
 
 		require 'footer-view.php';
+		includeExternalFoot();
 
 	 ?>
-		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
-</body>
+
+	</body>
 
 </html>
