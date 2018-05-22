82565fa639c5fde7de411f72feb467b1dc7fcb08
Phillip Redfern <philllip347@gmail.com>
2018-05-22 16:57:23 +0200

    connexion refait
    tout en bootstrap et réarrangé

--- view/connexion-view.php
+++ view/connexion-view.php
@@ -18,73 +18,48 @@
 
 <main>
 
-
-	<h4>Veuillez entrez vos identifiants et mot de passe:</h4>
-<div class="container">
-
-		<div class="flex-container">
-
-			<form method="post" action="connexion.php">
-				<div class="row">
-					<div class="col-xs-1 col-lg-0">
-
+	<div class="row">
+		<div class="col-xs-1 col-lg-0"></div>
+		<div class="col-xs-10 col-lg-12">
+			<div class="row">
+				<div class="col-xs-0 col-lg-2"></div>
+				<div class="col-xs-12 col-lg-8">
+					<div class="content">
+						<h4>Veuillez entrer vos identifiants</h4>
 					</div>
-					<div class="col-xs-10 col-lg-12">
-
-					</div>
-					<div class="col-xs-1 col-lg-0">
-
-					</div>
-
-				</div>
-
-				<fieldset class="fieldset-form">
-
-						<legend>Vos identifiants</legend>
-						<div class="form" action="inscription.php"  method="post">
-								<div class="flex-input">
-									<label for="email">Email :</label>
+					<form class="" action="connexion.php" method="post">
+						<div class="content">
+							<fieldset class="fieldset-form">
+								<legend>Vos identifiants</legend>
+									<label  for="email">Email :</label>
 									<input type="email" name="email" placeholder="Ex: exemple@email.fr" <?php echo 'value="' . $emailInput . '"'; ?> id="email" required>
-								</div>
-
-								<div class="flex-input">
-									<label for="password">Mot de passe :</label>
+									<label  for="password">Mot de passe :</label>
 									<input type="password" name="password" id="password" required>
-								</div>
-
+							</fieldset>
 						</div>
-
-			</fieldset>
-
-				<input class="ConnectionBouton" type="submit" name="ConnectionBouton" value="Se Connecter">
-
-				<div class="checkboxArea">
-						 <input class="checkbox" type="checkbox" name="resteConnecte" value="resteConnecte" id="resteConnecte" />
-						 <label for="resteConnecte">Rester connecté</label>
+						<div class="content">
+							<input class="connectionButton" type="submit" name="ConnectionBouton" value="Se Connecter">
+							<div class="stayConnected">
+								<input  type="checkbox" name="resteConnecte" value="resteConnecte" id="resteConnecte" />
+								<label for="resteConnecte">Rester connecté</label>
+							</div>
+						</div>
+					</form>
+					<form class="" action="inscription.php" method="post">
+						<div class="content">
+							<p>vous n'avez pas encor de compt?</p>
+						</div>
+						<div class="content">
+							<input class="createAcc" type="submit" name="button" value="Créer un compte"/>
+						</div>
+					</form>
 				</div>
-
-			</form>
-
-			<form class="" action="inscription.php" method="post">
-
-				<div class="linkInscription">
-
-					<p>Vous n'avez pas encore de compte?</p>
-
-					<input class="CreateAcc" type="submit" name="button" value="Créer un compte"/>
-
-				</div>
-
-			</form>
-
-
+				<div class="col-xs-0 col-lg-2"></div>
+			</div>
 		</div>
+		<div class="col-xs-1 col-lg-0"></div>
+	</div>
 
-		<div class="clearfix"></div>
-
-
-
-</div>
 
 
 </main>
