475df1d6e0d97be4a39824f2d707ae800f65ef74
Phillip Redfern <philllip347@gmail.com>
2018-05-28 17:53:17 +0200

    css side nav progress

--- view/side-nav-view1.php
+++ view/side-nav-view1.php
@@ -4,16 +4,14 @@
     <div class="nav-info-content">
 
 
-    <fieldset class="fieldset-side">
-
+      <div class="phone-grid">
       <div class="phoneLabel">
           <span>Telephone:</span>
       </div>
       <div class="phoneNumber">
           <span>07 68 48 10 02</span>
       </div>
-
-    </fieldset>
+    </div>
     <button type="button" class="RdvBandeau btn .btn-primary btn-sm">Prendre Rendez-Vous</button>
     </div>
   </div>
