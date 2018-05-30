6bd45c0138c62aa74934bfd7b169cf9b8c524977
Phillip Redfern <philllip347@gmail.com>
2018-05-30 13:29:57 +0200

    js...

--- models/pieces/head_external.php
+++ models/pieces/head_external.php
@@ -8,3 +8,4 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.10/angular.min.js"></script>
+<script src="../assets/js/module/module.lcqr.js" type="text/javascript"></script>
